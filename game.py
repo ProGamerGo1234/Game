220539ff730956d45e67455fbcbc7a632a43410c
ProGamerGo1234 <martin.takac47@gmail.com>
2026-01-09 10:29:47 +0400

    Finished the base game
    Fix player movement logic and update display calls.

--- game.py
+++ game.py
@@ -5,3 +5,29 @@ screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+
+player_location = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+     running = False
+
+  screen.fill("lime")
+
+  pygame.draw.circle(screen, "blue", player_location, 40)
+  
+  key = pygame.keys.get_pressed()
+  if key[pygame.K_w]:
+    player_location.y = -= 300 * dt
+  if key[pygame.K_s]:
+    player_location.y = += 300 * dt
+  if key[pygame.K_a]:
+    player_location.x = -= 300 * dt
+  if key[pygame.K_d]:
+    player_location.x = += 300 * dt
+    
+  pygame.display.flip
+  dt = clock.tick(60) / 1000
+
+pygame.flip
