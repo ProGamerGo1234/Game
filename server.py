c6a1b1c0906761f7d23efede170159abf5279d00
ProGamerGo1234 <martin.takac47@gmail.com>
2026-01-09 19:06:52 +0400

    Update server.py
    Added player counting functionality to the server.

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import time
 print('Server Started!')
 number = 0
-players = 0
 while True:
   print(f'Server: {number}th loop')
+  print('Counting players...')
+  players_raw = open('players.txt')
+  players = int(players_raw)
+  print(f'There are {players} players online')
+  time.sleep(10)
   
   
   
